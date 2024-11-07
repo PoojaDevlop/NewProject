522c28a4cab85462439a6b0043e746b583feee54
PoojaDevlop <poojanira2019@gmail.com>
2024-11-07 15:12:38 +0530

    New commit

--- NewProject/src/org/project/NewBio.java
+++ NewProject/src/org/project/NewBio.java
@@ -5,8 +5,9 @@ public static void main (String args[]){
 	System.out.println("Name: Pooja");
 	System.out.println("Age:25");
 	System.out.println("Gender : Female");
-		System.out.println("Place : Chennai");
-	Sysytem.out.println("Native: thanjavur");
+	System.out.println("Place : Chennai");
+	System.out.println("Native: thanjavur");
+	System.out.println("Qualification: B.sc.,");
 	
 }
 }
