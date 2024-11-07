532c5eb1cda3bbc8e15be05e7b551ed5dcd4f17c
PoojaDevlop <poojanira2019@gmail.com>
2024-11-07 15:56:14 +0530

    update hobby NewBio.java

--- NewProject/src/org/project/NewBio.java
+++ NewProject/src/org/project/NewBio.java
@@ -6,7 +6,7 @@ public static void main (String args[]){
 	System.out.println("Age:25");
 	System.out.println("Gender : Female");
 		System.out.println("Place : Chennai");
-	Sysytem.out.println("Native: thanjavur");
-	
+	Sytem.out.println("Native: thanjavur");
+	Sytem.out.println("hobby: cooking");
 }
 }
