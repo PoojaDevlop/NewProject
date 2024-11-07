ee377dcef8c6d514733c96f366860b76d79dc11d
PoojaDevlop <poojanira2019@gmail.com>
2024-11-07 11:42:05 +0530

    commit file to dev

---
+++ NewProject/src/org/project/NewBio.java
@@ -0,0 +1,10 @@
+package org.project;
+
+public class NewBio {
+public static void main (String args[]){
+	System.out.println("Name: Pooja");
+	System.out.println("Age:25");
+	Sytem.out.println("Gender : Female");
+		Sytem.out.println("Place : Chennai");
+}
+}
